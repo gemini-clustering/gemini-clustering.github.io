0e27f937b940356c91708eea4e43ec8f63847707
oshillou <louis.ohl@inria.fr>
2024-03-13 11:06:00 +0000

    CircleCI update of dev docs (80).

---
+++ _downloads/70f74f786f862f4b66b6af3a441551f3/plot_kernel_rim.py
@@ -0,0 +1,49 @@
+"""
+===================================
+Clustering circles with kernel RIM
+===================================
+
+We show here a simple dataset consisting in two centred circle that can be challenging for some clustering algorithms.
+We solve this case using the kernel RIM algorithm. This algorithm fits a logistic regression on the kernel
+matrix derived from the dataset.
+"""
+from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn import metrics
+
+from gemclus.linear import KernelRIM
+
+#######################################
+# Draw samples for the circle dataset
+# -------------------------------------
+
+# %%data
+
+# We start by generating samples distributed on two circles
+noise = 0.05
+factor = 0.1
+X, y = datasets.make_circles(n_samples=200, noise=noise, factor=factor, random_state=0)
+
+#############################
+# Training clustering model
+# ---------------------------
+
+# %%clustering
+model_kernel = KernelRIM(n_clusters=2, base_kernel="laplacian", max_iter=1000, reg=4/len(X), random_state=0)
+y_pred = model_kernel.fit_predict(X)
+print(f"ARI = {metrics.adjusted_rand_score(y, y_pred)}")
+
+######################################
+# Show predictions on similar samples
+# ------------------------------------
+
+# Create a novel set of samples and cluster them
+new_X, new_y = datasets.make_circles(n_samples=200, noise=noise, factor=factor, random_state=1)
+new_pred = model_kernel.predict(new_X)
+print(f"ARI = {metrics.adjusted_rand_score(new_y, new_pred)}")
+
+
+plt.scatter(X[:, 0], X[:, 1], c=y_pred, marker="+", label="Training samples")
+plt.scatter(new_X[:, 0], new_X[:, 1], c=new_pred, marker="o", label="Testing samples")
+plt.legend()
+plt.show()
